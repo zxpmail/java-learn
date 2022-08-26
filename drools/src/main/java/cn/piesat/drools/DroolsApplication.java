c42408cb127f4383b3fc5dbcdb6d013374aa748e
zxpmail@163.com <zxpmail@163.com>
2022-08-26 15:34:29 +0800

    初始化drools工程

---
+++ drools/src/main/java/cn/piesat/drools/DroolsApplication.java
@@ -0,0 +1,16 @@
+package cn.piesat.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhouxp
+ */
+@SpringBootApplication
+public class DroolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsApplication.class, args);
+    }
+
+}
---
+++ drools/src/test/java/cn/piesat/drools/DroolsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.piesat.drools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
